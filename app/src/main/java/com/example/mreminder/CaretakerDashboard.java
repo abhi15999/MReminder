dd66235e67d02a94ab8577db50143dc22bdd9d31
Cdivyam <divyamchoudhary@hotmail.com>
2020-11-18 17:27:21 +0530

    CaretakerDashboard made. Few tweaks hera and there

--- app/src/main/java/com/example/mreminder/CaretakerDashboard.java
+++ app/src/main/java/com/example/mreminder/CaretakerDashboard.java
@@ -1,14 +1,77 @@
 package com.example.mreminder;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class CaretakerDashboard extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class CaretakerDashboard extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar toolbar;
+    NavigationView navigationView;
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_caretaker_dashboard);
+
+        toolbar = findViewById(R.id.caretaker_toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_caretaker);
+        navigationView = findViewById(R.id.navigation_view_caretaker);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+        Log.i("I am here", String.valueOf(id));
+
+        if (id == R.id.home_caretaker) {
+            Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.alert_caretaker) {
+            Toast.makeText(this, "Alerts", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.profile_caretaker) {
+            Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.logout_btn_caretaker) {
+            logOut();
+            return true;
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void logOut() {
+        FirebaseAuth.getInstance().signOut();
+        sendToMain();
+    }
+
+    private void sendToMain() {
+
+        Intent ToMain = new Intent(CaretakerDashboard.this, MainActivity.class);
+        startActivity(ToMain);
+        finish();
     }
 }
